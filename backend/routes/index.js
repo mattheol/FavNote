713ef8cc1d923bb66a0518f9a2dcd8b81a4e0754
Mateusz Olsztyński <matiol1@hotmail.com>
2020-02-25 16:23:22 +0100

    Created Note schema model in backend

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const user = require("../controllers/UserController");
+const note = require("../controllers/NoteController");
 
 const router = express.Router();
 
 router.post("/user/login", user.userLogin);
 router.post("/user/logout", user.userLogout);
 router.post("/user/register", user.userRegister);
+
+router.get("/notes", note.getAllNotes);
+router.get("/notes/type", note.getAllNotesOfOneType);
+
+router.post("/note", note.addNote);
+router.get("/note/:id", note.getSingleNote);
+router.put("/note/:id", note.updateNote);
+router.delete("/note/:id", note.deleteNote);
 
 module.exports = router;
